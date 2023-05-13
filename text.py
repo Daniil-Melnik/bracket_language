1aeee0ee455d3b8cb24b470e0ee35771a82b7507
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2023-05-14 02:51:13 +0300

    pochti finally

--- text.py
+++ text.py
@@ -1,7 +1,7 @@
 #str = "{(a+b-c)(a-c)}+(a+b)-[(k+t)(a+b)]"
-str = input(":= ")
 
-print("input: "+str+'\n')
+
+#print("input: "+str+'\n')
 
 dict={
     "{" : "A",
@@ -25,6 +25,12 @@ arr_E=[]
 arr_F=[]
 
 arr_sign = ['-', '*', '/', '+']
+
+print("\n\nСинтаксический анализатор грамматики, основанной на трёх видах скобок с применением букв и знаков операций.")
+print("\n\nФормулировка правил: Правильная скобочная запись арифметических выражений с тремя видами скобок. \nВнутри фигурных скобок обязательно должны быть квадратные, но могут быть круглые, внутри квадратных должны быть круглые или бесскобочные выражения, внутри круглых только бесскобочные арифметические выражения.\nТакже, если один из операндов является скобкой, то и второй должен быть скобкой (не может быть буквой).\nМогут быть “лишние” скобки, но одна буква не может браться в скобки.\n\n")
+
+str = input("Введите запись: ")
+
 
 k=0
 str_1=[]
@@ -66,7 +72,7 @@ for i in range(0, len(str_1)):
 #print(str_1)
 
 if (len(arr_A)!=len(arr_B))or(len(arr_C)!=len(arr_D))or(len(arr_E)!=len(arr_F)):
-    print("Error, it is not a correct bracket sequence")
+    print("ОШИБКА: скобочная запись не является правильной")
     cond_1 = False
 i=0
 for i in range(0, len(str)):
@@ -75,32 +81,32 @@ for i in range(0, len(str)):
             i=+7
         else:
             cond_6=False
-            print("проблема со знаками", i)
+            print("ОШИБКА: расстановка знаков оераций некорректна", i)
 
 for i in range(0, len(arr_E)):
     for j in range (arr_E[i]+1, arr_F[i]):
         if (str_1[j]=='E' and str_1[j-1]!='E')or(str_1[j]=='A')or(str_1[j]=='B')or(str_1[j]=='C')or(str_1[j]=='D'):
             cond_2 = False
-            print("внутри круглых скобок есть скобочные записи или круглые нелишние")
-            print(j)
+            print("ОШИБКА: внутри круглых скобок есть скобочные записи или круглые нелишние")
+            #print(j)
 if (cond_1 and cond_2 and cond_6):
     for i in range(0, len(arr_E)):
         if (arr_F[i]-arr_E[i]==2):
             cond_5 = False
-            print("есть однобуквенные записи внутри круглых скобок")
+            print("ОШИБКА: есть однобуквенные записи внутри круглых скобок")
 if (cond_1 and cond_2 and cond_5 and cond_6):
     for i in range(0, len(arr_C)):
         for j in range(arr_C[i]+1, arr_D[i]):
             if (str_1[j]=='C' and str_1[j-1]!='C')or(str_1[j]=='A'):
                 cond_3=False
-                print("внутри квадратных скобок есть фигурные или квадратные нелишние")
+                print("ОШИБКА: внутри квадратных скобок есть фигурные или квадратные нелишние")
 
 if (cond_1 and cond_2 and cond_3 and cond_5 and cond_6):
     for i in range (0, len(arr_A)):
         for j in range(arr_A[i]+1, arr_B[i]):
             if (str_1[j]=='A' and str_1[j-1]!='A'):
                 cond_4=False
-                print("внутри фигурных скобок есть фигурные нелишние")
+                print("ОШИБКА: внутри фигурных скобок есть фигурные нелишние")
                 
 if (cond_1 and cond_2 and cond_3 and cond_5 and cond_6 and cond_4):
     for i in range (0, len(arr_A)):
@@ -110,31 +116,31 @@ if (cond_1 and cond_2 and cond_3 and cond_5 and cond_6 and cond_4):
                 t_c=True
         if (t_c==False):
             cond_7=False
-            print("внутри фигурных нет обязательных квадратных")
+            print("ОШИБКА: внутри фигурных нет обязательных квадратных")
             
 if (cond_1 and cond_2 and cond_3 and cond_5 and cond_6 and cond_4 and cond_7):
     for i in range (0, len(arr_A)):
         t_c = 0
         for p in range(0, len(arr_C)):
             if(arr_C[p]>arr_A[i]):
-                t_c+=arr_D[p]-arr_C[p]+1
+                t_c+=arr_D[p]-arr_C[p]
                 #print("квадратная ", t_c)
                 
         for p in range(0, len(arr_E)):
             if(arr_E[p]>arr_A[i]):
-                t_c+=arr_F[p]-arr_E[p]+1
+                t_c+=arr_F[p]-arr_E[p]
                 #print("круглая ", t_c)
                 
         for p in range(0, len(str)):
             if (str[p]in arr_sign)and(str[p-1]in[')',']','}'])and(str[p+1]in ['(','[','{']):
                 t_c+=1
                 #print("знак ",t_c)
-                
-        if (t_c < arr_B[i]-arr_A[i]-1):
-            print("внутри фигурных скобок есть бесскобочные выражения", t_c, arr_B[i]-arr_A[i])
+        #print(arr_B[i]-arr_A[i]+1)      
+        if (t_c < arr_B[i]-arr_A[i]+1):
+            print("ОШИБКА: внутри фигурных скобок есть бесскобочные выражения")
             cond_8 = False
 
                 
 if (cond_1 and cond_2 and cond_3 and cond_4 and cond_5 and cond_6 and cond_7 and cond_8):
-    print("the bracket entry corresponds to the given grammar")
+    print("Введённая скобочная запись соответствует заданной грамматике")
 
