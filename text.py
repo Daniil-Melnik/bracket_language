25e5d04b82bef5710de5fbc411d023e0fb00060c
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2023-05-14 01:12:02 +0300

    new type of detecting

--- text.py
+++ text.py
@@ -1,4 +1,4 @@
-str = "{[(a+b)][ab](a+m)}/[([a+m-(d+t)])]"
+str = "{[(a+b)]-[a+b]+(a+m)}/[(a+m-d+t)]"
 
 print("input: "+str+'\n')
 
@@ -23,7 +23,7 @@ arr_D=[]
 arr_E=[]
 arr_F=[]
 
-arr_sign = []
+arr_sign = ['-', '*', '/', '+']
 
 k=0
 str_1=[]
@@ -39,7 +39,12 @@ for i in range(0, len(str)):
     if (str[i] in ['{','}','[',']','(',')']):
         str_1[i] = dict[str[i]]
         
-cond_1 = True
+cond_1 = True # все скобки на месте
+cond_2 = True # внутри круглых норма
+cond_3 = True # внутри квадратных норма
+cond_4 = True # внутри фигурных норма
+cond_5 = True # нет однобуквенных в круглых
+cond_6 = True # проблема с расстановкой букв и знаков
         
 for i in range(0, len(str_1)):
     if str_1[i]=='A':
@@ -54,95 +59,47 @@ for i in range(0, len(str_1)):
         arr_E.append(i)
     elif str_1[i]=="F":
         arr_F.append(i)
+    
+print(str_1)
 
 if (len(arr_A)!=len(arr_B))or(len(arr_C)!=len(arr_D))or(len(arr_E)!=len(arr_F)):
     print("Error, it is not a correct bracket sequence")
     cond_1 = False
+i=0
+for i in range(0, len(str)):
+    if (str[i] in arr_sign):
+        if (((str[i-1]in ch)or(str[i-1]in['{','}','[',']','(',')']))and((str[i+1]in ch)or(str[i+1]in['{','}','[',']','(',')']))):
+            i=+7
+        else:
+            cond_6=False
+            print("проблема со знаками", i)
 
-if (cond_1):
-    cond_2 = True
-    for i in range(0, len(arr_A)):
-        cond_fig_square = False
-        for j in range(arr_A[i], arr_B[i]):
-            if (str_1[j]=='C' or str_1[j]=='D'):
-                cond_fig_square=True
-                
-        if (cond_fig_square==False):
-            print("Error with figure-squre brackets")
+for i in range(0, len(arr_E)):
+    for j in range (arr_E[i]+1, arr_F[i]):
+        if (str_1[j]=='E' and str_1[j-1]!='E')or(str_1[j]=='A')or(str_1[j]=='B')or(str_1[j]=='C')or(str_1[j]=='D'):
             cond_2 = False
-
-    for i in range(0, len(arr_A)):
-        cond_fig_fig = True
-        for j in range(arr_A[i]+1, arr_B[i]):
-            if (str_1[j]=='A' or str_1[j]=='B'):
-                cond_fig_fig=False
-                
-        if (cond_fig_fig==False):
-            print("Error with figure-figure brackets")
-            cond_2 = False
-            
-    for i in range(0, len(arr_A)):
-        cond_fig_empty = True
-        for j in range(arr_A[i]+1, arr_B[i]):
-            if (str_1[j]=='A' or str_1[j]=='B'or str_1[j]=='C' or str_1[j]=='D' or str_1[j]=='E' or str_1[j]=='F'):
-                cond_fig_empty=False
-                
-        if (cond_fig_empty==True):
-            print("Error with figure-poor brackets")
-            cond_2 = False
-            
-    for i in range(0, len(arr_C)):
-        cond_square_circl = False
-        for j in range(arr_C[i]+1, arr_D[i]):
-            if (str_1[j]=='E' or str_1[j]=='F'):
-                cond_square_circl=True
-                
-        if (cond_square_circl==False):
-            print("Error with scuare-circl brackets")
-            cond_2 = False
-   
-    for i in range(0, len(arr_C)):
-            cond_square_square = False
-            for j in range(arr_C[i]+1, arr_D[i]):
-                if (str_1[j]=='C' or str_1[j]=='D'):
-                    cond_square_square=True
-                    
-            if (cond_square_square==True):
-                print("Error with scuare-square brackets")
-                cond_2 = False
-                
-    for i in range(0, len(arr_C)):
-            cond_square_fig = False
-            for j in range(arr_C[i]+1, arr_D[i]):
-                if (str_1[j]=='A' or str_1[j]=='B'):
-                    cond_square_fig=True
-                    
-            if (cond_square_fig==True):
-                print("Error with scuare-figure brackets")
-                cond_2 = False
-
+            print("внутри круглых скобок есть скобочные записи или круглые нелишние")
+            print(j)
+if (cond_1 and cond_2 and cond_6):
     for i in range(0, len(arr_E)):
-            cond_circle_empty = False
-            for j in range(arr_E[i]+1, arr_F[i]):
-                if (str_1[j]=='A' or str_1[j]=='B'or str_1[j]=='C' or str_1[j]=='D' or str_1[j]=='E' or str_1[j]=='F'):
-                    cond_circle_empty=True
-                    
-            if (cond_circle_empty==True):
-                print("Error with circle-empty brackets")
-                cond_2 = False
-    if cond_2:
-        cond_3=True
-        for i in range(0, len(str)):
-            if str[i] in ['+','-','*','/']:
-                arr_sign.append(i)
-        for i in range(0, len(arr_sign)):
-            cond_term = False
-            if ((str[arr_sign[i]+1] in ch) or (str[arr_sign[i]+1] in bracket_1))and((str[arr_sign[i]-1] in ch) or (str[arr_sign[i]-1] in bracket_2)):
-                cond_term = True
-            if (cond_term == False):
-                print("Error vith term")
+        if (arr_F[i]-arr_E[i]==2):
+            cond_5 = False
+            print("есть однобуквенные записи внутри круглых скобок")
+if (cond_1 and cond_2 and cond_5 and cond_6):
+    for i in range(0, len(arr_C)):
+        for j in range(arr_C[i]+1, arr_D[i]):
+            if (str_1[j]=='C' and str_1[j-1]!='C')or(str_1[j]=='A'):
                 cond_3=False
+                print("внутри квадратных скобок есть фигурные или квадратные нелишние")
+
+if (cond_1 and cond_2 and cond_3 and cond_5 and cond_6):
+    for i in range (0, len(arr_A)):
+        for j in range(arr_A[i]+1, arr_B[i]):
+            if (str_1[j]=='A' and str_1[j-1]!='A'):
+                cond_4=False
+                print("внутри фигурных скобок есть фигурные нелишние")
+
                 
-if (cond_1 and cond_2 and cond_3):
+if (cond_1 and cond_2 and cond_3 and cond_4 and cond_5 and cond_6):
     print("the bracket entry corresponds to the given grammar")
 
